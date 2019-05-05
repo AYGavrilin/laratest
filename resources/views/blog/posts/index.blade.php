d1454d51dfe2063fe471630207105e5593d6ceee
aleksandr <aleksandr2525@gmail.com>
2019-05-05 22:47:43 +0300

    Lesson 11

--- resources/views/blog/posts/index.blade.php
+++ resources/views/blog/posts/index.blade.php
@@ -1,9 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
 <table class="table">
     @foreach($items as $item)
-        <tr>
+        <tr class="table-hover">
             <td>{{ $item->id }}</td>
             <td>{{ $item->title }}</td>
             <td>{{ $item->created_at }}</td>
         </tr>
     @endforeach
 </table>
+@endsection
